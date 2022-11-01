3c201b942e6b1adf906ca891f1649ea68b6b3c24
Yujan Ranjitkar <63731234+0xYujan@users.noreply.github.com>
2022-11-01 22:11:08 +0545

    Create Example2.sol

---
+++ Error/Example2.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Error {
+    function testRequire(uint _i) public pure {
+        
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint _i) public pure {
+        
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint public num;
+
+    function testAssert() public view {
+        
+        assert(num == 0);
+    }
+
+    // custom error
+    error InsufficientBalance(uint balance, uint withdrawAmount);
+
+    function testCustomError(uint _withdrawAmount) public view {
+        uint bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({balance: bal, withdrawAmount: _withdrawAmount});
+        }
+    }
+}
